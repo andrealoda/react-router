d43a303d2d5a85ed582e13c58367fd39e0f62200
andrea8980 <andrea@casaloda.it>
2026-05-15 22:35:56 +0200

    install react-router-dom, bootstrap, bootstrap-icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 
 function App() {
 
   return (
     <>
+
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout />}>
+
+          <Route path="/" element={<HomePage />} />
+         
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
     </>
   )
 }
